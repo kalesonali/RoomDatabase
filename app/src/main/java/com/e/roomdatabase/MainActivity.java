5374696406fbe87cbf18310515c303fce211f7ce
kalesonali <kalesonali9011@gmail.com>
2020-02-10 15:01:02 +0530

    Initial commit

---
+++ app/src/main/java/com/e/roomdatabase/MainActivity.java
@@ -0,0 +1,19 @@
+package com.e.roomdatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        createdb();
+    }
+
+    private void createdb(){
+
+    }
+}
